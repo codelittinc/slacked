997f2791eb5de5f30dcbceeeb329d83510cf28dc
sean <seathony@gmail.com>
2015-12-07 17:43:41 -0500

    fixed pathing to .env file and removed from rails spec

--- spec/post_console_spec.rb
+++ spec/post_console_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe ".env" do
-  it 'should exist in root' do
-    expect(File).to be_file(".env")
+  it 'should exist in install' do
+    expect(File).to be_file("lib/generators/slacked/install/templates/.env")
   end
 
   it 'should contain SLACK_WEBHOOK' do
-    expect(File.read(".env")).to include "SLACK_WEBHOOK"
+    expect(File.read("lib/generators/slacked/install/templates/.env")).to include "SLACK_WEBHOOK"
   end
 
   it 'should contain SLACK_MESSAGE' do
-    expect(File.read(".env")).to include "SLACK_MESSAGE"
+    expect(File.read("lib/generators/slacked/install/templates/.env")).to include "SLACK_MESSAGE"
    end
 end
