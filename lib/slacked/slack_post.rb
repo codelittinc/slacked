724d0d5d446d082934b8e4ddcc6e1ba5b079ad8c
TomasBarry <tomas.barry@hotmail.com>
2019-07-10 13:10:36 +0100

    Allow for dynamic webhooks to be used by Slacked
    This commit introduces the ability for a user to set a dynamic webhook
    url through the use of the `config` parameter to `Slacked#post` and
    `Slacked#post_async`. By introducing this feature through the `config`
    parameter, there are no breaking changes with previous versions of
    Slacked.
    
    The behaviour of Slacked is exactly the same as before, where Slacked
    will use the default `ENV[SLACK_WEBHOOK_URL_KEY]` set in the `.env`
    file. However, should you want to use `Slacked` to send messages to
    different webhooks from a single Rails app, you can now do so:
    
    ```ruby
    Slacked.post('My message to the default webhook') # this sends a message
    to the default webhook url set by `ENV[SLACK_WEBHOOK_URL_KEY]` in the
    `.env` file
    
    Slacked.post('My message to a specific webhook url', { webhook_url:
    '<SOME_WEBHOOK_URL>'}) # this sends a message to an explicit webhook url
    ```
    
    The README and tests have been updated accordingly.

--- lib/slacked/slack_post.rb
+++ lib/slacked/slack_post.rb
@@ -1,15 +1,20 @@
 module Slacked
   SLACK_PROFILE_IMAGE=':robot_face:'
-  SLACK_WEBHOOK_URL_KEY='SLACK_WEBHOOK'
+  SLACK_WEBHOOK_URL_KEY = 'SLACK_WEBHOOK'
   SLACK_DEFAULT_MESSAGE_KEY='SLACK_DEFAULT_MESSAGE'
-  SLACK_DEFAULT_CONFIG= {
-      icon_emoji: SLACK_PROFILE_IMAGE
-  }
+  SLACK_DEFAULT_CONFIG = {
+    icon_emoji: SLACK_PROFILE_IMAGE,
+    webhook_url: ENV[SLACK_WEBHOOK_URL_KEY]
+  }.freeze
 
   class << self
     def post message = ENV[SLACK_DEFAULT_MESSAGE_KEY], config = SLACK_DEFAULT_CONFIG
       return false if message.nil? || message.empty? || disabled?
-      slack_notifier.ping message, SLACK_DEFAULT_CONFIG.merge(config)
+
+      merged_configs = SLACK_DEFAULT_CONFIG.merge(config)
+      webhook_url = merged_configs.fetch(:webhook_url, SLACK_WEBHOOK_URL_KEY)
+      slack_notifier(webhook_url)
+        .ping(message, merged_configs)
     end
 
     def post_async message= ENV[SLACK_DEFAULT_MESSAGE_KEY], config = SLACK_DEFAULT_CONFIG
@@ -27,8 +32,9 @@ module Slacked
     end
 
     private
-    def slack_notifier webhook_url = ENV[SLACK_WEBHOOK_URL_KEY]
-      Slack::Notifier.new webhook_url
+
+    def slack_notifier(webhook_url)
+      Slack::Notifier.new(webhook_url)
     end
 
     def rails?
